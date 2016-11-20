e5caf3803c2a42946f002f43ee167814dd0f24b5
Jake Ojero <jakeojero@hotmail.com>
2016-11-20 14:14:36 -0500

    Hash and Salt Password

--- server.js
+++ server.js
@@ -150,15 +150,15 @@ app.post('/users', function (req, res) {
 	db.user.create(body)
 	.then ( function (user) {
 			if(user)
-				res.json(user.toJSON());
+				res.json(user.toPublicJSON());
 	})
 	.catch ( function (e) {
 			res.status(400).json(e);
 	});
-	
+
 });
 
-db.sequelize.sync()
+db.sequelize.sync(/*{force: true}*/)
 .then( function () {
 		app.listen(PORT, function () {
 				console.log('Express listening on port ' + PORT + '!');
