fa7f95e672a03e81b4a9664f9c46209dce34b750
Jake Ojero <jakeojero@hotmail.com>
2016-11-20 00:04:37 -0500

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -63,6 +63,37 @@ app.delete('/todos/:id', function(req, res) {
 
 });
 
+// PUT /todos/:id
+app.put('/todos/:id', function(req, res) {
+		var todoId = parseInt(req.params.id, 10);
+		var match = _.findWhere(todos, {id: todoId});
+		var body = _.pick(req.body, 'completed', 'description');
+		var validAttributes = {};
+
+		if(!match) {
+				return res.status(404).send();
+		}
+
+		if( body.hasOwnProperty('completed') && _.isBoolean(body.completed) ) {
+				validAttributes.completed = body.completed;
+		}
+		else if( body.hasOwnProperty('completed') ) {
+				return res.status(400).send();
+		}
+
+		if( body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0 ) {
+				validAttributes.description = body.description.trim();
+		}
+		else if( body.hasOwnProperty('description') ) {
+				return res.status(400).send();
+		}
+
+		// UPDATE HERE
+		_.extend(match, validAttributes);
+		res.json(match);
+
+});
+
 app.listen(PORT, function () {
 		console.log('Express listening on port ' + PORT + '!');
 });
